1fb0e3cc07912f50570a4f9774e77cdb69cdc242
quanwei0 <weiquan0128@gmail.com>
2025-03-21 15:31:16 -0500

    add remax trainer

--- verifiers/__init__.py
+++ verifiers/__init__.py
@@ -5,6 +5,7 @@ from .envs.math_env import MathEnv
 from .envs.simple_env import SimpleEnv
 from .envs.tool_env import ToolEnv
 from .trainers.grpo_env_trainer import GRPOEnvTrainer
+from .trainers.remax_env_trainer import ReMaxEnvTrainer
 from .utils.data_utils import extract_boxed_answer, extract_hash_answer, preprocess_dataset
 from .utils.model_utils import get_model, get_tokenizer, get_model_and_tokenizer
 from .utils.config_utils import get_default_grpo_config
@@ -24,6 +25,7 @@ __all__ = [
     "SimpleEnv",
     "ToolEnv",
     "GRPOEnvTrainer",
+    "ReMaxEnvTrainer",
     "get_model",
     "get_tokenizer",
     "get_model_and_tokenizer",
